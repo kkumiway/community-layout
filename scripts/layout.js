002319c3a407f3e26cb97862b207ab9b3afe1eac
Haein <haeinjo@postech.ac.kr>
2025-03-18 14:46:36 +0900

    chore: minor changes

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("/components/header.html") // 헤더 HTML을 가져옴
+    fetch("../components/header.html") // 헤더 HTML을 가져옴
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-placeholder").innerHTML = data;
